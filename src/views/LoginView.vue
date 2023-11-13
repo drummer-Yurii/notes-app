a2d6a0a733fe9c5b3ff5704933842aa5b94df1e3
Yurii Rybachok <drummer2009@ukr.net>
2023-11-13 11:51:04 +0200

    add data & func for Login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useSignInEmailPassword, useSignUpEmailPassword } from '@nhost/vue';
+
+const router = useRouter();
+
+const isRegister = ref(false);
+
+const email = ref('');
+const password = ref('');
+
+const { signUpEmailPassword } = useSignUpEmailPassword();
+const { signInEmailPassword } = useSignInEmailPassword();
+
+const registerOrLogin = async async => {
+    if (!email.value || !password.value) {
+        return alert("Please fill in all fiealds!")
+    }
+
+    const res = isRegister.value
+        ? await signUpEmailPassword(email.value, password.value)
+        : await signInEmailPassword(email.value, password.value)
+
+    if (res.isError) return alert(res.error.message)
+    router.push('/');
+}
+</script>
+
 <template>
     <main>
         Login
