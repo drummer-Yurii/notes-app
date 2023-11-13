75b602dc82e7172610057dd0c286d3d79a83adec
Yurii Rybachok <drummer2009@ukr.net>
2023-11-13 12:09:43 +0200

    create form for login user

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -29,6 +29,36 @@ const registerOrLogin = async async => {
 
 <template>
     <main>
-        Login
+        <h1 class="text-4xl font-bold mb-8">My Notes App</h1>
+        <form @submit.prevent="registerOrLogin">
+            <h3 class="text-xl font-bold uppercase mb-4">Login or Register</h3>
+            <label class="block mb-4">
+                <input type="checkbox" v-model="isRegister" />Register?
+            </label>
+            <label class="block mb-4">
+                <span class="block text-sm uppercase mb-2">Email</span>
+                <input 
+                    type="email" 
+                    v-model="email" 
+                    placeholder="Enter your email"
+                    class="block w-full text-slate-800 px-4 py-2"
+                />
+            </label>
+            <label class="block mb-4">
+                <span class="block text-sm uppercase mb-2">Password</span>
+                <input 
+                    type="password" 
+                    v-model="password" 
+                    placeholder="Enter your password"
+                    class="block w-full text-slate-800 px-4 py-2"
+                />
+            </label>
+
+            <input 
+                type="submit" 
+                value="Login or Signup"
+                class="text-green-500 hover:underline cursor-pointer" 
+            />
+        </form>
     </main>
 </template>
